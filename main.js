a913ca88cc6a1699ceff07fee2b1a5a2100d8d96
racop <rahulcoolranjan@gmail.com>
2023-07-11 22:24:33 +0530

    modal for calendly

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ function displayImages() {
   images[index-1].style.display = "block";
   setTimeout(displayImages, 2000); 
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const openModalBtn = document.getElementById('openModalBtn');
+  const modal = document.getElementById('modal');
+  const closeModal = document.querySelector('.close');
+
+  openModalBtn.addEventListener('click', function () {
+    modal.style.display = 'block';
+    // loadCalendlyEvents();
+  });
+
+  closeModal.addEventListener('click', function () {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+
+});
