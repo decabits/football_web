c3d5405d4ae8ad94bb09e676ca57b86729a444d1
racop <rahulcoolranjan@gmail.com>
2023-10-25 11:49:44 +0530

    Added contact mail

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -46,7 +46,7 @@ $(function () {
         type: "POST",
         data: {
           to: `info@decabits.com`,
-          cc: `sahilbathla1@gmail.com`,
+          cc: `contact@trainwithlefty.com,sahilbathla1@gmail.com`,
           from: '"Decabits" info@decabits.com',
           subject: "TWL Contact Form:  " + name,
           text: `You have received a new message from TWL(Football club) contact form.\n\nHere are the details:\n\nName: ${name}\n\nMobile: ${phone}\n\nEmail: ${email}\n\nMessage:\n${message}`,
